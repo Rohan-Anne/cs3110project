e36808f7e3cfe83917f23dbaf725826f879cd7fc
erics118 <52634785+erics118@users.noreply.github.com>
2026-04-20 00:45:48 -0400

    hide impl of chunk

--- lib/chunk.ml
+++ lib/chunk.ml
@@ -1,20 +1,16 @@
-(** chunk object *)
 type t = {
   x : int;  (** chunk x coordinate *)
   y : int;  (** chunk y coordinate *)
   z : int;  (** chunk z coordinate *)
-  blocks : Block.t array;  (** flat array of chunk_size^3 of block types *)
+  blocks : Block.t array;  (** flat array of chunk_size^3 block types *)
 }
 
-(** [index x y z] gets the index in the [blocks] array of [Chunk.t] of the block
-    at the location (x, y, z) *)
+let x chunk = chunk.x
+let y chunk = chunk.y
+let z chunk = chunk.z
+
 let index x y z =
   x + (y * Config.chunk_size) + (z * Config.chunk_size * Config.chunk_size)
 
-(** [get chunk x y z] gets the block type of the block located at (x, y, z) in
-    [chunk] *)
 let get chunk x y z = chunk.blocks.(index x y z)
-
-(** [get chunk x y z v] sets the block type of the block located at (x, y, z) in
-    [chunk] to [v]*)
-let set chunk x y z (v : Block.t) = chunk.blocks.(index x y z) <- v
+let set chunk x y z v = chunk.blocks.(index x y z) <- v
---
+++ lib/chunk.mli
@@ -0,0 +1,21 @@
+(** chunk object *)
+type t
+
+(** [x chunk] returns the x coordinate of [chunk] *)
+val x : t -> int
+
+(** [y chunk] returns the y coordinate of [chunk] *)
+val y : t -> int
+
+(** [z chunk] returns the z coordinate of [chunk] *)
+val z : t -> int
+
+(** [index x y z] returns the flat array index for block [(x, y, z)] within a
+    chunk *)
+val index : int -> int -> int -> int
+
+(** [get chunk x y z] returns the block at [(x, y, z)] within [chunk] *)
+val get : t -> int -> int -> int -> Block.t
+
+(** [set chunk x y z v] sets the block at [(x, y, z)] within [chunk] to [v] *)
+val set : t -> int -> int -> int -> Block.t -> unit
