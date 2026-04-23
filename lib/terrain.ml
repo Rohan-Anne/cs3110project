4ab091d21a075e7ffef10e687c5725af57714fa3
Rohan <ra733@cornell.edu>
2026-04-23 19:32:38 -0400

    Finished definition for terrain.ml

--- lib/terrain.ml
+++ lib/terrain.ml
@@ -1,16 +1,26 @@
-(* this needs to be properly implemented based off of fill_chunk *)
-let height_at _x _z = 0
+let height_at x z =
+  let scale = 0.05 in
+  let n = Noise.perlin2d (float_of_int x *. scale) (float_of_int z *. scale) in
+  int_of_float (6.0 +. 4.0 *. n)
 
-(* basic chunk generation. todo: make this more complicated, using perlin
-   noise *)
-let fill_chunk ~cx:_ ~cy ~cz:_ =
+let fill_chunk ~cx ~cy ~cz =
   let cs = Config.chunk_size in
   let blocks = Array.make (cs * cs * cs) Block.Air in
-  (* generate a slope of grass blocks *)
-  if cy = 0 then
-    for bx = 0 to cs - 1 do
-      for bz = 0 to cs - 1 do
-        blocks.(Chunk.index bx (bz / 4) bz) <- Block.Grass
+  for bx = 0 to cs - 1 do
+    for bz = 0 to cs - 1 do
+      let wx = cx * cs + bx in
+      let wz = cz * cs + bz in
+      let h = height_at wx wz in
+      for by = 0 to cs - 1 do
+        let wy = cy * cs + by in
+        let block =
+          if wy > h then Block.Air
+          else if wy = h then Block.Grass
+          else if wy >= h - 3 then Block.Dirt
+          else Block.Stone
+        in
+        blocks.(Chunk.index bx by bz) <- block
       done
-    done;
+    done
+  done;
   blocks
