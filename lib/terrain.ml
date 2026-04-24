a4deb375f76dbaf13ef86baa0d83e71f128bc504
erics118 <52634785+erics118@users.noreply.github.com>
2026-04-23 20:01:59 -0400

    fmt

--- lib/terrain.ml
+++ lib/terrain.ml
@@ -1,18 +1,18 @@
 let height_at x z =
   let scale = 0.05 in
   let n = Noise.perlin2d (float_of_int x *. scale) (float_of_int z *. scale) in
-  int_of_float (6.0 +. 4.0 *. n)
+  int_of_float (6.0 +. (4.0 *. n))
 
 let fill_chunk ~cx ~cy ~cz =
   let cs = Config.chunk_size in
   let blocks = Array.make (cs * cs * cs) Block.Air in
   for bx = 0 to cs - 1 do
     for bz = 0 to cs - 1 do
-      let wx = cx * cs + bx in
-      let wz = cz * cs + bz in
+      let wx = (cx * cs) + bx in
+      let wz = (cz * cs) + bz in
       let h = height_at wx wz in
       for by = 0 to cs - 1 do
-        let wy = cy * cs + by in
+        let wy = (cy * cs) + by in
         let block =
           if wy > h then Block.Air
           else if wy = h then Block.Grass
