93c8842098ebe4e8032613f17cfa9d8561445886
erics118 <52634785+erics118@users.noreply.github.com>
2026-05-07 21:54:52 -0400

    slightly more bumpy

--- lib/terrain.ml
+++ lib/terrain.ml
@@ -1,7 +1,12 @@
 let height_at x z =
-  let scale = 0.05 in
-  let n = Noise.perlin2d (Float.of_int x *. scale) (Float.of_int z *. scale) in
-  Float.to_int (6.0 +. (4.0 *. n))
+  let x = Float.of_int x and z = Float.of_int z in
+  (* roughness for larger hills *)
+  let amp =
+    let roughness = (Noise.perlin2d (x *. 0.03) z +. 1.0) /. 2.0 in
+    roughness *. roughness
+  in
+  let detail = Noise.perlin2d (x *. 0.05) (z *. 0.05) in
+  Float.to_int (6.0 +. (15.0 *. amp *. detail))
 
 let fill_chunk ~cx ~cy ~cz =
   let cs = Config.chunk_size in
